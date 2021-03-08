d1bc666efd2817000c15bd402fb864560eb8419f
user <user@WINDOWS-VK4C0HF>
2021-03-08 22:58:12 +0100

    Add updating of Heroes.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,6 +13,10 @@ import { MessageService } from './message.service';
 export class HeroService {
   private heroesUrl = 'api/heroes';
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService
@@ -32,6 +36,14 @@ export class HeroService {
     return this.http.get<Hero[]>(this.heroesUrl).pipe(
       tap((_) => this.log('fetched heroes')),
       catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(() => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
