aa092fdfaa1181851ecb9726b6c52f51c0b102cd
user <user@WINDOWS-VK4C0HF>
2021-03-08 12:33:46 +0100

    Add routing to HeroDetail component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 // configures the routes
 const routes: Routes = [
   // adds default route (on app start)
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes', component: HeroesComponent },
 ];
 
 @NgModule({
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,6 +11,13 @@ import { MessageService } from './message.service';
 export class HeroService {
   constructor(private messageService: MessageService) {}
 
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    // of() returns Observable, thus makes the call async
+    return of(HEROES.find((hero) => hero.id === id));
+  }
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('Hero Service: fetched heroes');
